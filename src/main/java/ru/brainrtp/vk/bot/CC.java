cd5007bd67ecff12a616386432bf013c59dcd91b
BuildTools <unconfigured@null.spigotmc.org>
2017-12-24 21:53:45 +0500

    - Сделал что-то....

--- src/main/java/ru/brainrtp/vk/bot/CC.java
+++ src/main/java/ru/brainrtp/vk/bot/CC.java
@@ -5,17 +5,17 @@ package ru.brainrtp.vk.bot;
  */
 public class CC {
     // Reset
-    public static final String RESET = "\033[0m";  // Text Reset
+    static final String RESET = "\033[0m";  // Text Reset
 
     // Regular Colors
     public static final String BLACK = "\033[0;30m";   // BLACK
-    public static final String RED = "\033[0;31m";     // RED
-    public static final String GREEN = "\033[0;32m";   // GREEN
+    static final String RED = "\033[0;31m";     // RED
+    static final String GREEN = "\033[0;32m";   // GREEN
     public static final String YELLOW = "\033[0;33m";  // YELLOW
     public static final String BLUE = "\033[0;34m";    // BLUE
     public static final String PURPLE = "\033[0;35m";  // PURPLE
-    public static final String CYAN = "\033[0;36m";    // CYAN
-    public static final String WHITE = "\033[0;37m";   // WHITE
+    static final String CYAN = "\033[0;36m";    // CYAN
+    static final String WHITE = "\033[0;37m";   // WHITE
 
     // Bold
     public static final String BLACK_BOLD = "\033[1;30m";  // BLACK
@@ -58,7 +58,7 @@ public class CC {
     public static final String WHITE_BRIGHT = "\033[0;97m";  // WHITE
 
     // Bold High Intensity
-    public static final String BLACK_BOLD_BRIGHT = "\033[1;90m"; // BLACK
+    static final String BLACK_BOLD_BRIGHT = "\033[1;90m"; // BLACK
     public static final String RED_BOLD_BRIGHT = "\033[1;91m";   // RED
     public static final String GREEN_BOLD_BRIGHT = "\033[1;92m"; // GREEN
     public static final String YELLOW_BOLD_BRIGHT = "\033[1;93m";// YELLOW
--- src/main/java/ru/brainrtp/vk/bot/Main.java
+++ src/main/java/ru/brainrtp/vk/bot/Main.java
@@ -142,8 +142,7 @@ public class Main {
                                 break;
                             }
                         }
-                    }
-                    catch (Exception ex) {}
+                    } catch (Exception ignored) {}
                 } while (enable);
             }
         }, "ConsoleThread").start();
